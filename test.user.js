f199af53e137ec85a492af69e2d795eaa70fb17b
badhaloninja <jacobpatrickcrawford@gmail.com>
2017-09-29 09:39:10 -0500

    Create test.user.js

---
+++ test.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var $a = $(a);
+        if ($a.attr("href").startsWith("https://gbatemp.net")) {
+        $a.attr("src", $a.attr("src").replace("https://gbatemp.net", "http://62.210.180.159"));
+        }
+      $("a[href='http://gbatemp.net']").attr('href','http://62.210.180.159');
+      $("a[href='https://gbatemp.net/*']").replace('https://gbatemp.net','http://62.210.180.159');
+})();
