93650865627dc81952ca5e8a75f18f72aa82b924
badhaloninja <jacobpatrickcrawford@gmail.com>
2017-10-04 09:45:24 -0500

    Update ImgGo.user.js

--- ImgGo.user.js
+++ ImgGo.user.js
@@ -4,7 +4,7 @@
 // @website      https://github.com/badhaloninja/Userscrips
 // @icon         https://raw.githubusercontent.com/badhaloninja/Userscrips/master/Icons/icon.png
 // @icon64       https://raw.githubusercontent.com/badhaloninja/Userscrips/master/Icons/icon64.png
-// @version      0.2
+// @version      0.3
 // @updateURL    https://github.com/badhaloninja/Userscrips/raw/master/ImgGo.user.js
 // @description  Bypasses image weblock by using duckduckgo
 // @author       badhaloninja
@@ -19,10 +19,11 @@ $("img").each(function(ignore, e) {
     if ($e.attr("src").endsWith(".png")) {
     if ($e.attr("src").startsWith("http://")) {
         $e.attr("src", "https://images.duckduckgo.com/iu/?u=" + $e.attr("src").replace("http", "https"));
-    }}
-    if ($e.attr("src").endsWith(".png")) {
+    }
+    else
+//    if ($e.attr("src").endsWith(".png")) {
     if ($e.attr("src").startsWith("https://")) {
         $e.attr("src", "https://images.duckduckgo.com/iu/?u=" + $e.attr("src"));
-    }}
+    }
 });
 })();
