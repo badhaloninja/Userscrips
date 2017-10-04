6a682f279ddc066263fc523775d196af1b7f60cb
badhaloninja <jacobpatrickcrawford@gmail.com>
2017-10-04 09:41:26 -0500

    Create ImgGo.user.js

---
+++ ImgGo.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         ImgGo
+// @namespace    badhaloninja_ImgGo
+// @website      https://github.com/badhaloninja/Userscrips
+// @icon         https://raw.githubusercontent.com/badhaloninja/Userscrips/master/Icons/icon.png
+// @icon64       https://raw.githubusercontent.com/badhaloninja/Userscrips/master/Icons/icon64.png
+// @version      0.2
+// @updateURL    https://github.com/badhaloninja/Userscrips/raw/master/ImgGo.user.js
+// @description  Bypasses image weblock by using duckduckgo
+// @author       badhaloninja
+// @match        http://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+$("img").each(function(ignore, e) {
+    var $e = $(e);
+    if ($e.attr("src").endsWith(".png")) {
+    if ($e.attr("src").startsWith("http://")) {
+        $e.attr("src", "https://images.duckduckgo.com/iu/?u=" + $e.attr("src").replace("http", "https"));
+    }}
+    if ($e.attr("src").endsWith(".png")) {
+    if ($e.attr("src").startsWith("https://")) {
+        $e.attr("src", "https://images.duckduckgo.com/iu/?u=" + $e.attr("src"));
+    }}
+});
+})();
